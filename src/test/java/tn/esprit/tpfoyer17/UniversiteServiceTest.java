6f62b69833c2443032f618dd855af4644e765a78
Mohamed amine Tlili <foxweb585@gmail.com>
2025-10-25 09:49:52 +0100

    add jenkins

--- src/test/java/tn/esprit/tpfoyer17/UniversiteServiceTest.java
+++ src/test/java/tn/esprit/tpfoyer17/UniversiteServiceTest.java
@@ -4,6 +4,9 @@ package tn.esprit.tpfoyer17;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.*;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import tn.esprit.tpfoyer17.entities.Foyer;
 import tn.esprit.tpfoyer17.entities.Universite;
 import tn.esprit.tpfoyer17.repositories.FoyerRepository;
@@ -14,8 +17,12 @@ import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 class UniversiteServiceTest {
+
 
     @Mock
     private UniversiteRepository universiteRepository;
