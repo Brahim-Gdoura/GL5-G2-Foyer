cb8b81aa0813bbae591cb3d7e25148bf3a1bb41d
Mohamed Amine Tlili <92816414+mohamedamine585@users.noreply.github.com>
2025-11-09 00:04:34 +0100

    Update variables.tf

--- awstest/terraform/variables.tf
+++ awstest/terraform/variables.tf
@@ -1,1 +1,12 @@
+# variables.tf
+variable "project_name" {
+  description = "Nom du projet pour le tagging"
+  type        = string
+  default     = "k3s-jenkins"
+}
 
+variable "aws_region" {
+  description = "Région AWS"
+  type        = string
+  default     = "us-east-1"
+}
