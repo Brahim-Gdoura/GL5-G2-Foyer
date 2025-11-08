b63c78c45c291345e3677b3541025e8085835f5d
Mohamed Amine Tlili <92816414+mohamedamine585@users.noreply.github.com>
2025-11-08 23:23:52 +0100

    Update variables.tf

--- awstest/terraform/variables.tf
+++ awstest/terraform/variables.tf
@@ -1,19 +1,47 @@
-variable "region" {
-  default = "us-east-1"
+variable "aws_region" {
+  description = "Région AWS où déployer les ressources"
+  type        = string
+  default     = "us-east-1"
 }
 
-variable "cluster_name" {
-  default = "mykubernetes"
-}
-
-variable "eks_role_arn" {
-  default = "arn:aws:iam::624037120855:role/c180773a4650446l12557190t1w624037-LabEksClusterRole-rp3eprhLxjnh"
+variable "project_name" {
+  description = "Nom du projet"
+  type        = string
+  default     = "tp-k8s-demo"
 }
 
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+  description = "CIDR block pour le VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidrs" {
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+variable "instance_type" {
+  description = "Type d'instance EC2 (free tier)"
+  type        = string
+  default     = "t2.micro"  # Free tier eligible
+}
+
+variable "cluster_name" {
+  description = "Nom du cluster EKS"
+  type        = string
+  default     = "demo-eks-cluster"
+}
+
+variable "desired_capacity" {
+  description = "Nombre de nodes dans le cluster"
+  type        = number
+  default     = 1  # Minimum pour économiser
+}
+
+variable "max_capacity" {
+  description = "Nombre maximum de nodes"
+  type        = number
+  default     = 1
+}
+
+variable "min_capacity" {
+  description = "Nombre minimum de nodes"
+  type        = number
+  default     = 1
 }
