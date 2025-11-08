582973a41cbcbe3c8fe43ab3083430e029d40fd6
Mohamed Amine Tlili <92816414+mohamedamine585@users.noreply.github.com>
2025-11-08 21:45:22 +0100

    Update variables.tf

---
+++ awstest/terraform/variables.tf
@@ -0,0 +1,22 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+  default     = "mykubernetes"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
