8d40f213b5de3e17f081e352a38417eeaf897243
Mohamed Amine Tlili <92816414+mohamedamine585@users.noreply.github.com>
2025-11-08 22:14:52 +0100

    Update variables.tf

--- awstest/terraform/variables.tf
+++ awstest/terraform/variables.tf
@@ -1,25 +1,19 @@
 variable "region" {
-  description = "AWS region"
-  default     = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "cluster_name" {
-  description = "Name of the EKS cluster"
-  default     = "mykubernetes"
+  default = "mykubernetes"
+}
+
+variable "eks_role_arn" {
+  default = "arn:aws:iam::624037120855:role/c180773a4650446l12557190t1w624037-LabEksClusterRole-rp3eprhLxjnh"
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  default     = "10.0.0.0/16"
+  default = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
-  description = "List of public subnet CIDRs"
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-# If you want to use a pre-existing IAM role, set this
-variable "eks_role_arn" {
-  description = "ARN of existing IAM role for EKS cluster"
-  default     = ""  # leave empty if Terraform should create a new role
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
