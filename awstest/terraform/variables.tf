f222b7af14d8edb2894072244f14eefe20ac1c30
Mohamed Amine Tlili <92816414+mohamedamine585@users.noreply.github.com>
2025-11-08 21:53:22 +0100

    Update variables.tf

--- awstest/terraform/variables.tf
+++ awstest/terraform/variables.tf
@@ -1,22 +1,22 @@
 variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "cluster_name" {
-  description = "EKS cluster name"
-  type        = string
-  default     = "mykubernetes"
+  default = "mykubernetes"
 }
 
 variable "vpc_cidr" {
-  description = "VPC CIDR block"
-  default     = "10.0.0.0/16"
+  default = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
-  description = "Public subnet CIDRs"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+# ARN of a pre-existing IAM role for EKS (provided by AWS Academy lab)
+variable "eks_role_arn" {
+  description = "Pre-existing IAM role ARN for EKS"
+  type        = string
 }
