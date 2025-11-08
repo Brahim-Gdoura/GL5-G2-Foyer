fc254239c4b10aff8acc1103f3350b42d0ad9619
Mohamed Amine Tlili <92816414+mohamedamine585@users.noreply.github.com>
2025-11-08 22:01:14 +0100

    Update output.tf

--- awstest/terraform/output.tf
+++ awstest/terraform/output.tf
@@ -1,7 +1,7 @@
-output "cluster_endpoint" {
-  value = aws_eks_cluster.k8s_cluster.endpoint
-}
-
 output "cluster_name" {
   value = aws_eks_cluster.k8s_cluster.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.k8s_cluster.endpoint
 }
