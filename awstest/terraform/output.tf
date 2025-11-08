2e87e37bd3b30020c37e84fe710faa3f6487a3bf
Mohamed Amine Tlili <92816414+mohamedamine585@users.noreply.github.com>
2025-11-08 23:25:44 +0100

    Add multiple outputs for AWS resources
    Add outputs for VPC ID, EC2 instance details, and EKS cluster information.

--- awstest/terraform/output.tf
+++ awstest/terraform/output.tf
@@ -1,7 +1,34 @@
-output "cluster_name" {
-  value = aws_eks_cluster.k8s_cluster.name
+output "vpc_id" {
+  description = "ID du VPC"
+  value       = aws_vpc.main.id
 }
 
-output "cluster_endpoint" {
-  value = aws_eks_cluster.k8s_cluster.endpoint
+output "ec2_instance_id" {
+  description = "ID de l'instance EC2"
+  value       = aws_instance.bastion.id
+}
+
+output "ec2_public_ip" {
+  description = "IP publique de l'instance EC2"
+  value       = aws_instance.bastion.public_ip
+}
+
+output "eks_cluster_name" {
+  description = "Nom du cluster EKS"
+  value       = aws_eks_cluster.main.name
+}
+
+output "eks_cluster_endpoint" {
+  description = "Endpoint du cluster EKS"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "eks_cluster_security_group_id" {
+  description = "Security group du cluster EKS"
+  value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+}
+
+output "configure_kubectl" {
+  description = "Commande pour configurer kubectl"
+  value       = "aws eks update-kubeconfig --region ${var.aws_region} --name ${aws_eks_cluster.main.name}"
 }
