b3c2e6b8f27d2c4b4c45ca3ab9fd9604c355772d
Brahim-Gdoura <brahim.gdoura2014@gmail.com>
2025-11-15 20:44:02 +0100

    update terraform main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 # --- VPC ---
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.19.0"
+  version = "5.0.0"  # Version plus récente
 
   name = "simple-vpc"
   cidr = "10.0.0.0/16"
@@ -21,25 +21,60 @@ module "vpc" {
   azs             = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  enable_dns_hostnames = true
+
+  # Tags requis pour EKS
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/simple-eks" = "shared"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/simple-eks" = "shared"
+  }
 }
 
 # --- EKS ---
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "18.29.5"        # exemple version récente stable
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.3"  # Version stable récente
+
   cluster_name    = "simple-eks"
   cluster_version = "1.28"
 
-  vpc_id      = module.vpc.vpc_id
-  vpc_subnets = module.vpc.private_subnets  # <-- correct
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets  # ✅ Corrigé
 
-  # Définition des nodes gérés
-  managed_node_groups = {
+  # Configuration du cluster
+  cluster_endpoint_public_access = true
+
+  # EKS Managed Node Group
+  eks_managed_node_groups = {  # ✅ Nom correct
     default = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-      instance_types   = ["t3.medium"]
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
+
+      # Disques
+      disk_size = 20
+
+      # Tags
+      tags = {
+        Environment = "dev"
+      }
     }
+  }
+
+  # Tags du cluster
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
   }
 }
